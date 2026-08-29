39e196035d742f259ba3cf7b4b8af89408e183ae
omarcherif1 <cherifomar909@gmail.com>
2026-08-29 19:22:16 +0100

    feat: ajout balise Google Search Console verification

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="7uun-FKks2UC-6SqJVleX0CyUFF7Jc3xv-zz3CYha1k" />
 
         <title inertia>{{ config('app.name', 'Chokri Benomrane') }}</title>
 
